be1fe999d4ffb9691370cf140c99643a5335c3f1
MGCA2021 <78860973+MGCA2021@users.noreply.github.com>
2021-05-19 14:02:28 -0300

    Add files via upload

---
+++ ac04/src/ex01_A/Entry.java
@@ -0,0 +1,11 @@
+package ex01_A;
+
+public interface Entry<K, V> {
+	// Retorna uma chave armazenada nesta entrada.
+	public K getKey();
+
+	// Retorna o valor armazenado nesta entrada.
+	public V getValue();
+
+	
+}
