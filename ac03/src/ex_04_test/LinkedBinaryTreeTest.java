2b914cc9248fb26e66592d95c2e17c5b2773ac13
MGCA2021 <78860973+MGCA2021@users.noreply.github.com>
2021-05-19 15:16:24 -0300

    Add files via upload

---
+++ ac03/src/ex_04_test/LinkedBinaryTreeTest.java
@@ -0,0 +1,14 @@
+package ex_04_test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class LinkedBinaryTreeTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
