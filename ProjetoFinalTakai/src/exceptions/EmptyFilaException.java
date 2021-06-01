23de66a4dd95261f12b6fb9a77ab135f98943fec
MGCA2021 <78860973+MGCA2021@users.noreply.github.com>
2021-05-31 21:15:32 -0300

    Delete ProjetoFinalTakai/src directory

--- ProjetoFinalTakai/src/exceptions/EmptyFilaException.java
+++
@@ -1,9 +0,0 @@
-package exceptions;
-
-@SuppressWarnings("serial")
-public class EmptyFilaException 
-    extends RuntimeException {
-    public EmptyFilaException(String erro) {
-        super(erro);
-    }
-}
--- ProjetoFinalTakai/src/exceptions/InvalidKeyException.java
+++
@@ -1,8 +0,0 @@
-package exceptions;
-@SuppressWarnings("serial")
-public class InvalidKeyException extends RuntimeException {
-
-	public InvalidKeyException(String err) { 
-		super(err); 
-		}
-}
